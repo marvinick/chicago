667010ee83964d1aecc750f0873d8e56092602dc
marvinick <marviluw@gmail.com>
2015-05-17 23:32:27 -0500

    rspec

---
+++ db/migrate/20150518042003_create_blog.rb
@@ -0,0 +1,9 @@
+class CreateBlog < ActiveRecord::Migration
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.text :content
+      t.timestamps
+    end
+  end
+end
---
+++ spec/features/user_adds_post_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+feature "User adds blog" do
+  scenario "successfully" do
+    visit root_path
+  end
+end
